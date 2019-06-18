dcecfc343c27b8bbc782a921c2529034e29721a0
Gyan chand Mohanty <gyanchand.mohanty@gmail.com>
2019-06-18 09:55:50 +0530

    literals means value

---
+++ 01_literals/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//integer literals
+	fmt.Println(
+		-1, 230, 55,
+	)
+
+	//float64 literals
+	fmt.Println(-.5, 1., 0.)
+
+	//bool
+	fmt.Println(true, false)
+
+	//string literals
+	fmt.Println(
+		"",
+		"gyan",
+		"",
+	)
+
+}
